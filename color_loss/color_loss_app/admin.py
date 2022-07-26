0c34f0ec75a45eedff8c69a3335518d46514f8c8
Kirill Ephanov <kirill.ephanov@gmail.com>
2022-07-26 19:18:03 +0300

    Profile and Clan models added

--- color_loss/color_loss_app/admin.py
+++ color_loss/color_loss_app/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'user_lvl', 'user_money', 'user_clan')
+    list_display_links = ('id', 'user', 'user_lvl', 'user_money', 'user_clan')
+    search_fields = ('user',)
+
+class ClanAdmin(admin.ModelAdmin):
+    list_display = ('id', 'clan_name', 'clan_description', 'clan_is_open')
+    list_display_links = ('id', 'clan_name', 'clan_description', 'clan_is_open')
+    search_fields = ('clan_name',)
+
+admin.site.register(Profile, ProfileAdmin)
+admin.site.register(Clan, ClanAdmin)
+
+admin.site.site_title = 'Color Loss management'
+admin.site.site_header = 'Color Loss management'
--- color_loss/color_loss_app/models.py
+++ color_loss/color_loss_app/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+class Profile(models.Model):
+    '''The class extends the user model'''
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user_lvl = models.IntegerField(default=1, blank=True, verbose_name='Player Level')
+    user_money = models.IntegerField(default=0, blank=True, verbose_name='Colors') # Game currency
+    user_clan = models.ForeignKey('Clan', on_delete=models.PROTECT, null=True, blank=True, verbose_name='Member of a clan')
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
+class Clan(models.Model):
+    '''Class displays clans inside the game'''
+    clan_name = models.CharField(max_length=50, db_index=True, verbose_name='Сlan name')
+    clan_description = models.TextField(max_length=250, blank=True, verbose_name='Description')
+    clan_is_open = models.BooleanField(blank=True, verbose_name='Is open')
+
+    def __str__(self):
+        return self.clan_name
+
+    class Meta:
+        verbose_name = 'Clan'
+        verbose_name_plural = 'Clans'
+        ordering = ['clan_name']
